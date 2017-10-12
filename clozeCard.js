deaf3a2e5e4b4b202364fdc062e666015ec1d308
Phil Kappaz <p.c.kappaz2@gmail.com>
2017-10-12 18:27:34 -0500

    initial push

---
+++ clozeCard.js
@@ -0,0 +1,31 @@
+function ClozeCard (text, cloze) {
+    this.fulltext = text;
+    this.cloze = cloze;
+    this.partial = funtion(){
+        return text.replace(cloze, "...");
+    };
+    
+};
+
+
+
+
+
+
+
+
+
+var firstPresidentCloze = new ClozeCard(
+    "George Washington was the first president of the United States.", "George Washington");
+
+// "George Washington"
+console.log(firstPresidentCloze.cloze);
+
+// " ... was the first president of the United States.
+console.log(firstPresidentCloze.partial); "
+
+// "George Washington was the first president of the United States.
+console.log(firstPresidentCloze.fullText): "
+
+// Should throw or log an error because "oops" doesn't appear in "This doesn't work"
+var brokenCloze = new ClozeCard("This doesn't work", "oops");
